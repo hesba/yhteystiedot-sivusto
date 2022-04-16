c7e0d315bdebea8747141a560f1c0a82c1b8e6bf
hesba <sillahe2@gmail.com>
2022-04-16 14:21:03 +0300

    testausta

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
  
 // We use Route in order to define the different routes of our application
-import { Route, Routes } from "react-router-dom";
- 
+//import { Route, Routes } from "react-router-dom";
+import { Container } from 'react-bootstrap'
+import NavBar from './components/NavBar'
+import Papers from "./components/Papers";
 // We import all the components we need in our app
 
  
 const App = () => {
  return (
-   <div>
-     
-   </div>
+   <Container>
+     <NavBar />
+     <Papers />
+   </Container>
  );
 };
  
