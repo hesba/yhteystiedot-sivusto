51f83839e33b420c94cfaf6c1a5feb2e4a95661b
hesba <sillahe2@gmail.com>
2022-04-23 12:51:26 +0300

    added body-parser. added nodemon

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@
 
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 const Yhteystiedot = require('./contact_person/cpmodel')
 const cors = require("cors")
 require("dotenv").config({ path: "./config.env" })
 const port = process.env.PORT || 5000
 app.use(cors())
-app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(require("./contact_person/cprouter"))
 // haetaan tietokantayhteys
 const dbo = require("./db/dbconn")
